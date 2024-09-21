a6e6e527479451bbf68394de17126e9bc2d49461
sahashiharshit <harshitsah96@gmail.com>
2024-09-21 15:49:31 +0530

    Express js middleware

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use((req,res,next)=>{
+   next();
+});
+app.use((req,res,next)=>{
+    res.send('<h1>Hello from express');
+});
+
+const server = http.createServer(app);
+server.listen(3000);
