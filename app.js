d6bc264449520d4ccba0085e1e8d0f81a518625e
sahashiharshit <harshitsah96@gmail.com>
2024-09-23 22:27:34 +0530

    Routes and 404"

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-const http = require('http');
-const express = require('express');
+const express = require("express");
 const bodyParser = require('body-parser');
-
 const app = express();
-app.use(bodyParser.urlencoded({extended:false}));
-app.use('/add-product', (req,res,next)=>{
-    res.send(`<h1>Add Product</h1><br><form action="/product" method="POST">Name <input type="text" name="product">
-        <br><br>Size <input type="number" name="size">
-        <button type="submit">Add Product</button></form>`);
-});
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-app.use('/', (req,res,next)=>{
-    res.send('<h1>Hello from express</h1>');
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
-
-const server = http.createServer(app);
-server.listen(3000);
+app.listen(3000);
