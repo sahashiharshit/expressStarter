d5f23852d2cab33531c9ebd1dfc6d799d117c8b9
sahashiharshit <harshitsah96@gmail.com>
2024-09-25 16:05:00 +0530

    Dynamic Routes

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+
+const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
 const app = express();
-const errorController = require('./controllers/errorControllers')
-app.set('view engine','ejs');
-app.set('views','views');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const contactRoutes = require("./routes/contact");
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname,'public')));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use('/admin',adminRoutes);
-app.use(contactRoutes);
 
-
-app.use(errorController._404page);
+app.use(errorController.get404);
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,21 @@
-const express = require("express");
+const path = require('path');
 
-const productsController = require("../controllers/products");
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
 const router = express.Router();
 
-router.get("/", productsController.getProductsList);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId',shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
