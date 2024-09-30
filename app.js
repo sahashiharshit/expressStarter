d14a2994a9be7f77036c3c780ff9e31952e2363b
sahashiharshit <harshitsah96@gmail.com>
2024-09-30 22:33:02 +0530

    Added sequelize in code

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-const path = require('path');
-const db = require('./util/database');
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require("path");
+const db = require("./util/database");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const errorController = require('./controllers/error');
-
+const errorController = require("./controllers/error");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+db.sync()
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
--- util/database.js
+++ util/database.js
@@ -1,9 +1,7 @@
-const mysql = require('mysql2');
-
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'harshit',
-    database:'shop',
-    password:'19962024'
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("shop", "harshit", "19962024", {
+  dialect: "mysql",
+  host: "localhost",
 });
-module.exports=pool.promise();
+
+module.exports = sequelize;
